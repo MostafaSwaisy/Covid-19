ecf51fe1c3fdfd4a57bdcb2cfb2daaba8037fc72
IsmailElkhalili <Ismail_Elkhalili@github.com>
2020-05-18 15:51:19 +0300

    second commit login action

---
+++ app/src/main/java/com/example/cov19/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.cov19;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
